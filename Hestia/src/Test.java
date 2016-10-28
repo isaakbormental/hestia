77144e1c3260f5dcb26407f9357a21cfbad82bbd
isaakbormental <nikita.dayanoff@yandex.ru>
2016-10-28 14:25:53 +0300

    sout

--- Hestia/src/Test.java
+++ Hestia/src/Test.java
@@ -1,7 +1,9 @@
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("LSDLFK");
-        System.out.println("LSDLFK");
+        System.out.println("Nikita");
+        System.out.println("Yannick");
+        System.out.println("");
+        System.out.println("");
     }
 }
