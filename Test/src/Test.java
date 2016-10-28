1fb496ae5cc80f9bf4a4fafcc89b0dea86145234
isaakbormental <nikita.dayanoff@yandex.ru>
2016-10-28 14:29:34 +0300

    sout

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -5,6 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("LSDLFK");
         System.out.println("LSDLFK");
-        System.out.println("new commit");
+
     }
 }
