b9bf652e19b2a4f31006b00329de771e1680adda
yannick-kouakam <ykuegue@yahoo.fr>
2016-10-28 14:19:18 +0300

    test

---
+++ Test/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kouakam on 28.10.16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("LSDLFK");
+        System.out.println("LSDLFK");
+    }
+}
