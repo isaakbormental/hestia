2bcbc7936ba22825e67ae43f9a00ad5ff83a6446
vladostan <vladostan94@gmail.com>
2016-10-29 09:28:36 +0300

    Test commit

--- Hestia/src/Test.java
+++ Hestia/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Nikita");
         System.out.println("Yannick");
+        System.out.println("Vladislav");
         System.out.println("");
         System.out.println("");
     }
